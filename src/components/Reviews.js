8eaf7a5d2b942c6254537cceb2873142498ab699
Your Name <rameselamese@hotmail.com>
2023-12-05 19:04:59 +1100

    Styled User Reviews

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { collection, query, onSnapshot } from 'firebase/firestore'
+import "../App.css"
 
 export function Reviews ( props ) {
   const db = useContext( FSContext )
@@ -30,18 +31,20 @@ export function Reviews ( props ) {
 
   const ReviewItems = reviews.map( (item) => {
     return (
+      <Container className="UserReview" style={{padding: 20, margin: 10}}>
       <Row>
         <Col>
           <h5>{item.title}</h5>
-          <strong>Rated {item.star} stars</strong><br/>
+          <strong>Rated {item.star} stars!</strong><br/>
           <em>{item.body}</em>
         </Col>
       </Row>
+      </Container>
     )
   })
 
   return(
-    <Container>
+    <Container className="UserReview">
       { ReviewItems }
     </Container>
   )
